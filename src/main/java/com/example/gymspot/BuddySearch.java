aec8149c00045b770de44be794c71af22f16b91f
Kawagga Kyewalyanga Naggenda <knaggenda@gmail.com>
2020-04-30 12:17:49 -0400

    Add files via upload

---
+++ src/main/java/com/example/gymspot/BuddySearch.java
@@ -0,0 +1,23 @@
+package com.example.gymspot;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class BuddySearch extends AppCompatActivity {
+    
+}
