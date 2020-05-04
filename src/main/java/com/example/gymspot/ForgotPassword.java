d86b49acf99a7fd9ed95026366f61e5c419d11ce
Kawagga Kyewalyanga Naggenda <knaggenda@gmail.com>
2020-05-04 11:23:21 -0400

    Update ForgotPassword.java

--- src/main/java/com/example/gymspot/ForgotPassword.java
+++ src/main/java/com/example/gymspot/ForgotPassword.java
@@ -7,16 +7,40 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class ResetActivity extends AppCompatActivity {
+public class ForgotPasswordActivity extends AppCompatActivity {
 
-
+    EditText userEmail;
+    Button userPass;
+    
+    FirebaseAuth firebaseAuth;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_reset);
+        setContentView(R.layout.activity_forgot_password);
 
-        email = (TextView)findViewById(R.id.reset_email);
-        btn =  (Button)findViewById(R.id.reset_button);
-        nAuth = FirebaseAuth. getInstance();
+        userEmail = findViewById(R.id.etuserEmail);
+        userPass = findViewById(R.id.btForgotPass);
+       
+        firebaseAuth =FirebaseAuth.getInstance();
+        
+        userPass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                firebaseAuth.sendPasswordResetEmail(userEmail.getText().toString())
+                    .addOnCompleteListener(new OnCompleteListener<Void>(){
+                  @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if(task.isSuccessful(){
+                            Toast.makeText(ForgotPasswordActivity.this, "Password sent to your Email", Toast.LENGTH_LONG.show();
+                        }else{
+                            Toast.makeText(ForgotPasswordActivity.this, task.getExcception.getMessage(), Toast.LENGTH_LONG.show();
+                          }
+                    }
+                });
+            }
+        });
+    }
+}
 
         
