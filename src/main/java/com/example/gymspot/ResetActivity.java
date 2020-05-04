255e93f476fc9715c4f1c0cdf24ebb6ac0710b89
Kawagga Kyewalyanga Naggenda <knaggenda@gmail.com>
2020-05-04 10:47:07 -0400

    Update ResetActivity.java

--- src/main/java/com/example/gymspot/ResetActivity.java
+++ src/main/java/com/example/gymspot/ResetActivity.java
@@ -1,8 +1,10 @@
-package com.example.gymapp;
+package com.example.gymspot;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class ResetActivity extends AppCompatActivity {
