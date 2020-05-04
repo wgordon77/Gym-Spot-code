868a4e9e0eb35f8fa1ab4c90d03b4ad44e91b280
Kawagga Kyewalyanga Naggenda <knaggenda@gmail.com>
2020-05-04 10:41:28 -0400

    Update ResetActivity.java

--- src/main/java/com/example/gymspot/ResetActivity.java
+++ src/main/java/com/example/gymspot/ResetActivity.java
@@ -1,4 +1,4 @@
-package com.example.gym;
+package com.example.gymapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -7,7 +7,7 @@ import android.widget.Button;
 
 public class ResetActivity extends AppCompatActivity {
     private TextView email;
-    private Button btn;
+    private Button F;
     private firebaseAuth mAuth;
 
     @Override
@@ -19,31 +19,4 @@ public class ResetActivity extends AppCompatActivity {
         btn =  (Button)findViewById(R.id.reset_button);
         nAuth = FirebaseAuth. getInstance();
 
-        btn.setOnClickListener(new View.OnClickListener(){
-            @Override
-                     public void onClick(View V)  {
-                String useremail = email.getText().toString().trim()
-
-                        if(TextUtils.isEmpty(useremail)){
-                            Toast.makeText(ResetActivity.this, "Please Enter Email", Toast.LENGTH_SHORT).show();
-                        }else{
-                            mAuth.sendPasswordResetEmail(useremail).addOnCompleteListener(new OnCompleteListener<Void>(){
-                                @Override
-                                public void onComplete(@NonNull Task <Void> task){
-                                    if(task.isSucessful()){
-                                        Toast.makeText(ResetActivity.this, "Check your Email \n and reset +" +
-                                                "your password", Toast.LENGTH_SHORT).show();
-                                        startActivity(new Intent(getApplicationContext(),MainActivity.class));
-                                    }else{
-                                        String message = task.getException().getMessage();
-                                        Toast.makeText(ResetActivity.this, "Error Occurred:" + message, Toast.LENGTH_SHORT).show();
-                                    }
-                                }
-                            })
-                        }
-
-            }
-        }
-        )
-    }
-}
+        
